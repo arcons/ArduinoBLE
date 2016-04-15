ccabf50683df5222d0c054c27c64d8873212e615
Alex Consunji <a.consunji@knights.ucf.edu>
2016-04-15 00:11:11 -0400

    No packet droping on BLEDADCOutTest

--- BLESetup/BLESetup.ino
+++ BLESetup/BLESetup.ino
@@ -1,12 +1,11 @@
 #include <SoftwareSerial.h>
 
-//hook these up properly you fool
 SoftwareSerial BTSerial(11, 10); //RX|TX
 
+
 void setup(){
-  Serial.begin(57600);
+  Serial.begin(9600);
   BTSerial.begin(9600); // default baud rate
-  //Serial.begin(9600);
   while(!Serial); //if it is an Arduino Micro
   Serial.println("AT commands: ");
 }
@@ -19,5 +18,5 @@ void loop(){
   //read from the Serial and print to the HM-10
   if(Serial.available())
     BTSerial.write(Serial.read());
-
 }
+
